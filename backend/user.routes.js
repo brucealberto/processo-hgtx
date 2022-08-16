fc126d7f5e24a1a79b14f18fa81cd10fffe8caeb
brucealberto <brucealberto2@gmail.com>
2022-08-16 09:35:43 -0300

    feat: update route

--- backend/user.routes.js
+++ backend/user.routes.js
@@ -5,5 +5,6 @@ const route = express.Router();
 
 route.post('/user', userController.create);
 route.get('/user', userController.getAll);
+route.put('/user/:id', userController.update)
 
 module.exports = route;
