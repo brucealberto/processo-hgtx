41800bfe9dd7265c30d7e7d66ba67dd9cd045c54
brucealberto <brucealberto2@gmail.com>
2022-08-17 16:10:01 -0300

    feat: token authorization

--- backend/user.routes.js
+++ backend/user.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const userController = require('./controllers/userController');
+const authToken = require('./middlewares/authToken');
 
 const route = express.Router();
 
 route.post('/user', userController.create);
-route.get('/user', userController.getAll);
+route.get('/user', authToken, userController.getAll);
 route.get('/user/:id', userController.getById);
 route.put('/user/:id', userController.update);
-route.delete('/user/:id', userController.deleteUser)
+route.delete('/user/:id', userController.deleteUser);
 
 module.exports = route;
