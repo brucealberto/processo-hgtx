0095d2eb36565f0ec019ac071325dffa46558471
brucealberto <brucealberto2@gmail.com>
2022-08-17 22:23:30 -0300

    feat: get user on frontend, fix css

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const route = require('./user.routes');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(route);
 
 module.exports = app;
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,5 +1,45 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from "react"
+
 export const Home = () => {
+  const [users, setUsers] = useState([]) 
+  useEffect(()=> {
+    async function getPosts() {
+     const { data } = await axios.get('http://localhost:3006/user')
+     setUsers(data);
+    }
+    getPosts()
+  }, [])
+
+
   return (
-    <h1>Tela Home</h1>
+    <div>
+      <div>
+           <table className='table'>
+              <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user)=> (
+                <tr>
+                  <th>{user.id}</th>
+                  <th>{user.name}</th>
+                  <th>{user.email}</th>
+                  <th>{user.cpf}</th>
+                </tr>
+              ))}
+            </tbody>
+           </table>    
+              <hr />
+
+      </div>
+    </div>
   )
 }
